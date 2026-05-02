93851dbd6da84580393486e2640492c1b93f1296
Omar Mokhtar <om.mokhtar.98@gmail.com>
2026-05-02 23:16:43 +0000

    change led sequence

--- main/main.c
+++ main/main.c
@@ -52,11 +52,11 @@ void led_blink_task(void *pvParameter)
     while (1)
     {
         gpio_set_level(LED_PIN_RED, 1);
-        gpio_set_level(LED_PIN_GREEN, 0);
+        gpio_set_level(LED_PIN_GREEN, 1);
         gpio_set_level(LED_PIN_BLUE, 0);
         vTaskDelay(pdMS_TO_TICKS(500));
 
-        gpio_set_level(LED_PIN_RED, 0);
+        gpio_set_level(LED_PIN_RED, 1);
         gpio_set_level(LED_PIN_GREEN, 1);
         gpio_set_level(LED_PIN_BLUE, 0);
         vTaskDelay(pdMS_TO_TICKS(500));
