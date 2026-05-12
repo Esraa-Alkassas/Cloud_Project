dad813d262fdd881b648f1ff1183ae953e23cceb
Omar Mokhtar <om.mokhtar.98@gmail.com>
2026-05-12 13:57:14 +0000

    Update LED pattern

--- main/main.c
+++ main/main.c
@@ -218,7 +218,7 @@ void led_blink_task(void *pvParameter)
         gpio_set_level(LED_PIN_RED, 1);
         gpio_set_level(LED_PIN_GREEN, 0);
         gpio_set_level(LED_PIN_BLUE, 0);
-        send_led_telemetry(1, 1, 0);
+        send_led_telemetry(1, 0, 0);
         vTaskDelay(pdMS_TO_TICKS(500));
         gpio_set_level(LED_PIN_RED, 0);
         gpio_set_level(LED_PIN_GREEN, 1);
